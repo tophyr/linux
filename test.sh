b9ca9ab871dc7326bf65a366ec48e9b5baa9705d
Chris Sarbora <tophyr@gmail.com>
2025-11-26 05:36:28 +0000

    fixup test.sh to be better out of the gate

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-g++ -O0 -g3 -Werror -pthread -static -Iproxy_user/include -std=c++23 proxy_user/main.cpp -Lproxy_user -lgtest -o RAMFS/bin/pu &&
+g++ -O0 -g3 -Werror -pthread -static -Iproxy_user/include -std=c++23 proxy_user/main.cpp ../googletest/build/lib/libgtest.a -o RAMFS/bin/pu &&
 pushd RAMFS/ &&
+chmod 600 root/private_data &&
+rm -rf dev/ &&
+mkdir dev/ &&
+sudo mknod dev/proxy c 242 0 &&
 find . | sudo cpio -oHnewc | gzip > ../initramfs.gz &&
 popd &&
+cp test_config .config &&
 make -j$(nproc) &&
 qemu-system-x86_64 -kernel arch/x86/boot/bzImage -initrd initramfs.gz -nographic -append "console=ttyS0" -m 512 --enable-kvm -cpu host -s
