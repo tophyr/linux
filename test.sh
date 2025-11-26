ba227db079afc291a7e3ee5e23f7ecfb208ac458
Chris Sarbora <tophyr@gmail.com>
2025-11-26 04:35:39 +0000

    fix busybox bins

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ g++ -O0 -g3 -Werror -pthread -static -Iproxy_user/include -std=c++23 proxy_user/
 pushd RAMFS/ &&
 find . | sudo cpio -oHnewc | gzip > ../initramfs.gz &&
 popd &&
-make -j96 &&
+make -j$(nproc) &&
 qemu-system-x86_64 -kernel arch/x86/boot/bzImage -initrd initramfs.gz -nographic -append "console=ttyS0" -m 512 --enable-kvm -cpu host -s
